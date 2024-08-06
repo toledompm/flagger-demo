7c09fff9da12ccfb614876b29426497c32cb6b30
marcos.toledo <marcos.toledo@wildlifestudios.com>
2024-08-06 19:24:54 -0300

    feat: allow all CORS origins

--- backend/main.go
+++ backend/main.go
@@ -19,7 +19,7 @@ func main() {
 
 	http.Handle("/", r)
 	fmt.Println("Server listening on port 8000")
-	originsOk := handlers.AllowedOrigins([]string{"http://localhost:3000"})
+	originsOk := handlers.AllowedOrigins([]string{"*"})
 	methodsOk := handlers.AllowedMethods([]string{"GET"})
 	http.ListenAndServe(":8000", handlers.CORS(originsOk, methodsOk)(r))
 }
